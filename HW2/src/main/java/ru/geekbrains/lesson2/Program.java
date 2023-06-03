7ca8a554ecd4b9e619611142b74fc4e2176c3bbc
Alexandr Sinkov <sinkov86@yandex.ru>
2023-06-03 12:30:32 +0300

    hw2 done

--- HW2/src/main/java/ru/geekbrains/lesson2/Program.java
+++ HW2/src/main/java/ru/geekbrains/lesson2/Program.java
@@ -9,6 +9,7 @@ package ru.geekbrains.lesson2;
 
 */
 
+import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -23,6 +24,7 @@ public class Program {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Random random = new Random();
     private static char[][] field; // Двумерный массив хранит текущее состояние игрового поля
+    private static char[][] testField; // Двумерный массив хранит тестовое состояние игрового поля
     private static int fieldSizeX; // Размерность игрового поля
     private static int fieldSizeY; // Размерность игрового поля
 
@@ -58,11 +60,13 @@ public class Program {
 
 
         field = new char[fieldSizeX][fieldSizeY];
+        testField = new char[fieldSizeX][fieldSizeY];
         // Пройдем по всем элементам массива
         for (int x = 0; x < fieldSizeX; x++) {
             for (int y = 0; y < fieldSizeY; y++) {
                 // Проинициализируем все элементы массива DOT_EMPTY (признак пустого поля)
                 field[x][y] = DOT_EMPTY;
+
             }
         }
     }
@@ -94,13 +98,37 @@ public class Program {
 
     }
 
+    private static void printTestField() {
+        System.out.println("TEST TEST TEST");
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++) {
+            System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
+        }
+        System.out.println();
+
+        for (int i = 0; i < fieldSizeX; i++) {
+            System.out.print(i + 1 + "|");
+
+            for (int j = 0; j < fieldSizeY; j++)
+                System.out.print(testField[i][j] + "|");
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < fieldSizeX * 2 + 2; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+    }
+
     /**
      * Обработка хода игрока (человек)
      */
     private static int[] humanTurn() {
         int x, y;
         do {
-            System.out.print("Введите координаты хода X и Y (от 1 до 3) через пробел >>> ");
+            System.out.print("Введите координаты хода X и Y через пробел >>> ");
             x = SCANNER.nextInt() - 1;
             y = SCANNER.nextInt() - 1;
         }
@@ -137,6 +165,51 @@ public class Program {
      */
     private static int[] aiTurn() {
         int x, y;
+        // Проверка победы игрока в следующем ходу
+        for (x = 0; x < fieldSizeX; x++) {
+            for (y = 0; y < fieldSizeY; y++) {
+
+                // копирование массива
+                for (int xx = 0; xx < fieldSizeX; xx++) {
+                    for (int yy = 0; yy < fieldSizeY; yy++) {
+                        testField[xx][yy] = field[xx][yy];
+                    }
+                }
+                if (testField[x][y] == DOT_EMPTY) {
+                    testField[x][y] = DOT_HUMAN;
+                    //printTestField();
+                    if (checkWin(DOT_HUMAN, new int[]{x, y}, WIN_COUNT, testField)) {
+                        System.out.println("AI видит возможность победы игрока и действует на упреждение!!!");
+                        field[x][y] = DOT_AI;
+                        return new int[]{x, y};
+                    }
+                }
+            }
+        }
+
+        // Проверка победы игрока через один ход
+        for (x = 0; x < fieldSizeX; x++) {
+            for (y = 0; y < fieldSizeY; y++) {
+
+                // копирование массива
+                for (int xx = 0; xx < fieldSizeX; xx++) {
+                    for (int yy = 0; yy < fieldSizeY; yy++) {
+                        testField[xx][yy] = field[xx][yy];
+                    }
+                }
+                if (testField[x][y] == DOT_EMPTY) {
+                    testField[x][y] = DOT_HUMAN;
+                    //printTestField();
+                    if (checkWin(DOT_HUMAN, new int[]{x, y}, WIN_COUNT-1, testField)) {
+                        System.out.println("AI видит возможность победы игрока и действует на упреждение!!!");
+                        field[x][y] = DOT_AI;
+                        return new int[]{x, y};
+                    }
+                }
+            }
+        }
+
+        // Если проверка не видит риски победы игрока в следующем ходу, то запускаем рандом
         do {
             x = random.nextInt(fieldSizeX);
             y = random.nextInt(fieldSizeY);
@@ -154,7 +227,7 @@ public class Program {
      * @param c
      * @return
      */
-    static boolean checkWin(char c, int[] tempArr) {
+    static boolean checkWin(char c, int[] tempArr, int wincount, char[][] fieldToTest) {
 
         int x = tempArr[0];
         int y = tempArr[1];
@@ -163,27 +236,27 @@ public class Program {
 
         // проверка по 8 сторонам
 
-        if (    eightWaysCheck(c,x,y,0,1) ||
-                eightWaysCheck(c,x,y,1,1) ||
-                eightWaysCheck(c,x,y,1,0) ||
-                eightWaysCheck(c,x,y,1,-1) ||
-                eightWaysCheck(c,x,y,0,-1) ||
-                eightWaysCheck(c,x,y,-1,-1) ||
-                eightWaysCheck(c,x,y,-1,0) ||
-                eightWaysCheck(c,x,y,-1,1)){
+        if (eightWaysCheck(c, x, y, 0, 1, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, 1, 1, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, 1, 0, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, 1, -1, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, 0, -1, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, -1, -1, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, -1, 0, wincount, fieldToTest) ||
+                eightWaysCheck(c, x, y, -1, 1, wincount, fieldToTest)) {
             return true;
         }
-
+        //System.out.println("AI не видит рисков");
         return false;
-}
+    }
 
-    static boolean eightWaysCheck(char c, int x, int y, int xi, int yi) {
+    static boolean eightWaysCheck(char c, int x, int y, int xi, int yi, int wincount, char[][] fieldToTest) {
         int j = 0;
-        for (int i = 0; i < WIN_COUNT; i++) {
-            if (!isCellValid(x + i * xi, y + i * yi) || !(field[x + i * xi][y + i * yi] == c)) {
+        for (int i = 0; i < wincount; i++) {
+            if (!isCellValid(x + i * xi, y + i * yi) || !(fieldToTest[x + i * xi][y + i * yi] == c)) {
                 return false;
             } else j++;
-            if (j == WIN_COUNT) return true;
+            if (j == wincount) return true;
         }
         return false;
     }
@@ -209,7 +282,7 @@ public class Program {
      * @return
      */
     static boolean gameCheck(char c, String str, int[] tempArr) {
-        if (checkWin(c, tempArr)) {
+        if (checkWin(c, tempArr, WIN_COUNT, field)) {
             System.out.println(str);
             return true;
         }
