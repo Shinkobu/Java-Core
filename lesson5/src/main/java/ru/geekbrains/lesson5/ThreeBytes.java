49c67ba45e82ce3c6e3d39296c0850904642e318
Alexandr Sinkov <sinkov86@yandex.ru>
2023-07-08 12:43:37 +0300

    HW5 done

--- lesson5/src/main/java/ru/geekbrains/lesson5/ThreeBytes.java
+++ lesson5/src/main/java/ru/geekbrains/lesson5/ThreeBytes.java
@@ -14,6 +14,7 @@ package ru.geekbrains.lesson5;
 
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -25,9 +26,9 @@ public class ThreeBytes {
 
     public static void main(String[] args) {
         try {
-//            SaveBytes(new int[]{1, 2, 3, 0, 2, 1, 3, 0, 0});
+            SaveBytes(new int[]{1, 2, 3, 0, 2, 1, 3, 0, 0});
 //            SaveBytes(new int[]{3, 3, 3, 3, 3, 3, 3, 3, 3});
-            SaveBytes(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0});
+//            SaveBytes(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0});
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -35,32 +36,45 @@ public class ThreeBytes {
 
     }
 
+    /**
+     * Основной метод, вызывает внутри encode и decode. Записывает закодированные значения в файл и считывает их
+     * @param data
+     * @throws IOException
+     */
     public static void SaveBytes(int[] data) throws IOException {
+
+        System.out.println("Исходый массив данных: " + Arrays.toString(data));
+
+        Byte[] myData = encode(data).toArray(new Byte[3]);
+        System.out.println("Данные закодированы в byte массив: " + Arrays.toString(myData));
 
         File myFile = new File("./threeBytesFile.txt");
         myFile.createNewFile();
-
-
-        Byte[] mydata = convert(data).toArray(new Byte[3]);
-        System.out.println(Arrays.toString(mydata));
+        System.out.println("Создан файл для выгрузки "  + myFile.getPath());
 
         try (FileOutputStream fileOutputStream = new FileOutputStream(myFile)) {
-            for (Byte b: mydata) {
+            for (Byte b : myData) {
                 fileOutputStream.write(b);
             }
+            System.out.println("Данные записаны в файл " + myFile.getPath());
 
         }
 
+        try (FileInputStream fileInputStream = new FileInputStream(myFile)) {
+            byte[] outputData = new byte[3];
+            fileInputStream.read(outputData, 0, myData.length);
+            System.out.println("Читаю данные из файла: " + Arrays.toString(outputData));
+            System.out.println("Данные декодированы: " + Arrays.toString(decode(outputData)));
+        }
 
-
-//        File inputFile = new File(filePath);
-//        byte[] data = new byte[inputFile.length()];
-//        FileInputStream fis = new FileInputStream(inputFile);
-//        fis.read(data, 0, data.length);
-//        fis.close();
     }
 
-    public static List<Byte> convert(int[] data) {
+    /**
+     * Кодирует числа из 4-ричной системы счисления в 10-ричную
+     * @param data
+     * @return на выходе имеем список из значений типа Byte
+     */
+    public static List<Byte> encode(int[] data) {
         if (!(data.length == 9)) throw new RuntimeException("Неверная длина входного массива!");
 
         List<Byte> fourDigitNumbersList = new ArrayList<>();
@@ -71,20 +85,50 @@ public class ThreeBytes {
             temp += data[i] * Math.pow(10, counter);
 
             if (counter == 0) {
-                System.out.println(temp);
-
+//                System.out.println(temp);
+//              конвертация из 4 ричной в 10 ричную систему счисления, чтобы впихнуть значение в тип данных byte
                 BigInteger a = new BigInteger(String.valueOf(temp), 4);
                 String s = a.toString(10);
                 //        BigInteger b = new BigInteger(s,2);
                 Byte b = Byte.valueOf(s);
-                System.out.println(b);
+//                System.out.println(b);
                 fourDigitNumbersList.add(b);
                 counter = 3;
                 temp = 0;
             }
 
         }
-        System.out.println(fourDigitNumbersList);
+//        System.out.println(fourDigitNumbersList);
         return fourDigitNumbersList;
     }
+
+    /**
+     * Декодирует числа из 10-ричной системы счисления в 4-ричную
+     * @param data
+     * @return
+     */
+    public static int[] decode(byte[] data) {
+        if (!(data.length == 3)) throw new RuntimeException("Неверная длина входного массива!");
+
+        int[] outputArray = new int[9];
+        int temp = 0;
+        for (int i = 0; i < data.length; i++) {
+//            counter--;
+            temp = data[i];
+//          декодирование из 10-ричной в 4-ричную
+            BigInteger a = new BigInteger(String.valueOf(temp), 10);
+            String s = a.toString(4);
+            int b = Integer.parseInt(s);
+//            System.out.println(b);
+            for (int j = 2; j >= 0; j--) {
+                outputArray[j + 3 * i] = b % 10;
+                b = b / 10;
+//                System.out.println(Arrays.toString(outputArray));
+
+            }
+        }
+        return outputArray;
+    }
+
+
 }
